7c85603e40ec480637efbc38c2336cd8699474cc
Dvooskid <dvooskid1234@gmail.com>
2024-06-28 05:52:47 +0100

    Add files via upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -125,7 +125,7 @@ const Navbar = () => {
               ],
               "team"
             )}
-{/*             <li className="nav-item">
+            {/* <li className="nav-item">
               <NavLink
                 className="nav-link hide-nav"
                 to="/career"
@@ -133,8 +133,8 @@ const Navbar = () => {
               >
                 Career
               </NavLink>
-            </li>
- */}
+            </li> */}
+
             <li className="nav-item">
               <a
                 className="nav-link hide-nav"
