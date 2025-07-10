9ced3533392aeaf3fbf7eac392e877cb2fcfe5b0
Dvooskid <dvooskid1234@gmail.com>
2025-07-10 12:27:59 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
@@ -17,7 +17,6 @@ import BoardOfDirectors from "./pages/team/BoardofDirectors";
 import BackgroundMusic from "./components/music/BackgroundMusic";
 import musicFile from "./assets/music/wonderful-world.mp3";
 import NotFound from "./pages/notfound/NotFound";
-
 import {
   Engineering,
   Liability,
@@ -28,10 +27,10 @@ import {
   Travel,
   SpecialtyLines,
 } from "./pages/product/index";
+import Chatbot from "./components/Chatbot";
+import WhatsappButton from "./components/WhatsappButton";
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
     AOS.init({
       duration: 1500,
@@ -40,95 +39,75 @@ const App = () => {
     });
   }, []);
 
-  useEffect(() => {
-    const preloader = document.getElementById("preloader");
-
-    setTimeout(() => {
-      preloader.style.display = "none";
-      setLoading(false);
-    }, 2500);
-  }, []);
-
   return (
     <div className="App bg-light">
-      {loading ? (
-        <div id="preloader" className="rotating-image"></div>
-      ) : (
-        <>
-          <Navbar />
-          <BackgroundMusic src={musicFile} />
-          <div className="fade">
-            <Routes>
-              <Route index exact path="/" element={<Home />} />
-              <Route exact path="/who-we-are" element={<About />} />
-              <Route exact path="/what-we-do" element={<Whatwedo />} />
-              <Route exact path="/our-products" element={<Products />} />
-              <Route
-                exact
-                path="/board-of-directors"
-                element={<BoardOfDirectors />}
-              />
-              <Route exact path="/management-team" element={<Team />} />
-              <Route
-                exact
-                path="/other-team-members"
-                element={<OtherTeamMembers />}
-              />
-              <Route exact path="/claim" element={<Claim />} />
-              <Route exact path="/report-a-claim" element={<Reportclaim />} />
-
-              <Route
-                exact
-                path="/products/life&personal-insurance"
-                element={<Life />}
-              />
-              <Route
-                exact
-                path="/products/engineering-insurance"
-                element={<Engineering />}
-              />
-              <Route
-                exact
-                path="/products/travel-insurance"
-                element={<Travel />}
-              />
-              <Route
-                exact
-                path="/products/pecuniary-insurance"
-                element={<Pecuniary />}
-              />
-              <Route
-                exact
-                path="/products/property-insurance"
-                element={<Property />}
-              />
-              <Route
-                exact
-                path="/products/liability-insurance"
-                element={<Liability />}
-              />
-              <Route
-                exact
-                path="/products/travel-insurance"
-                element={<Travel />}
-              />
-              <Route
-                exact
-                path="/products/motor-insurance"
-                element={<Motor />}
-              />
-              <Route
-                exact
-                path="/products/specialty-lines"
-                element={<SpecialtyLines />}
-              />
-              <Route path="*" element={<NotFound />} />
-              {/* Catch-all route for 404 */}
-            </Routes>
-          </div>
-          <Footer />
-        </>
-      )}
+      <Navbar />
+      <BackgroundMusic src={musicFile} />
+      <div className="fade">
+        <Routes>
+          <Route index exact path="/" element={<Home />} />
+          <Route exact path="/who-we-are" element={<About />} />
+          <Route exact path="/what-we-do" element={<Whatwedo />} />
+          <Route exact path="/our-products" element={<Products />} />
+          <Route
+            exact
+            path="/board-of-directors"
+            element={<BoardOfDirectors />}
+          />
+          <Route exact path="/management-team" element={<Team />} />
+          <Route
+            exact
+            path="/other-team-members"
+            element={<OtherTeamMembers />}
+          />
+          <Route exact path="/claim" element={<Claim />} />
+          <Route exact path="/report-a-claim" element={<Reportclaim />} />
+          <Route
+            exact
+            path="/products/life&personal-insurance"
+            element={<Life />}
+          />
+          <Route
+            exact
+            path="/products/engineering-insurance"
+            element={<Engineering />}
+          />
+          <Route
+            exact
+            path="/products/travel-insurance"
+            element={<Travel />}
+          />
+          <Route
+            exact
+            path="/products/pecuniary-insurance"
+            element={<Pecuniary />}
+          />
+          <Route
+            exact
+            path="/products/property-insurance"
+            element={<Property />}
+          />
+          <Route
+            exact
+            path="/products/liability-insurance"
+            element={<Liability />}
+          />
+          <Route
+            exact
+            path="/products/motor-insurance"
+            element={<Motor />}
+          />
+          <Route
+            exact
+            path="/products/specialty-lines"
+            element={<SpecialtyLines />}
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+      <Chatbot />
+      <WhatsappButton />
     </div>
   );
 };
