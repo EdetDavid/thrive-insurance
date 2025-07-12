db6173f8e13075df286498cc81261047a1d40ca9
Dvooskid <dvooskid1234@gmail.com>
2025-07-12 14:48:39 +0100

    Update WhatsappButton.jsx

--- src/components/WhatsappButton.jsx
+++ src/components/WhatsappButton.jsx
@@ -3,7 +3,9 @@ import { FaWhatsapp } from "react-icons/fa";
 import "./WhatsappButton.css";
 
 const whatsappNumber = "2347087942708"; // Replace with your WhatsApp number (country code + number, no + sign)
-const message = encodeURIComponent("Hello! I would like to know more about your travel services.");
+const message = encodeURIComponent(
+  "Hello, I'm interested in getting insurance but not sure where to start. Can you help me understand what type of insurance i need, how it works, and how much it costs?."
+);
 
 const WhatsappButton = () => (
   <a
